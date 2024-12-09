02e4234fa0990f8f7dcfa3dd885ca9c46f939384
HinaZeeshan <147984993+HinaZeeshan@users.noreply.github.com>
2024-12-09 23:06:30 +0500

    'modify'

--- src/Components/Collection.tsx
+++ src/Components/Collection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const products = [
   {
@@ -8,7 +9,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/filter.png", 
+    image: "/images/filter.png",
   },
   {
     id: 2,
@@ -17,7 +18,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image:"/images/filter (1).png", 
+    image: "/images/filter (1).png",
   },
   {
     id: 3,
@@ -26,7 +27,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/product-cover-5 (9).png", 
+    image: "/images/product-cover-5 (9).png",
   },
   {
     id: 4,
@@ -35,7 +36,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/media bg-cover.png", 
+    image: "/images/media bg-cover.png",
   },
   {
     id: 5,
@@ -44,7 +45,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/product-cover-5 (8).png", 
+    image: "/images/product-cover-5 (8).png",
   },
   {
     id: 6,
@@ -53,7 +54,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/product-cover-5 (11).png", 
+    image: "/images/product-cover-5 (11).png",
   },
   {
     id: 7,
@@ -62,8 +63,8 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/product-cover-5 (12).png", 
-    },
+    image: "/images/product-cover-5 (12).png",
+  },
   {
     id: 8,
     name: "Graphic Design",
@@ -71,7 +72,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/product-cover-5 (2).png", 
+    image: "/images/product-cover-5 (2).png",
   },
   {
     id: 9,
@@ -80,7 +81,7 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/fixed-height.png", 
+    image: "/images/fixed-height.png",
   },
   {
     id: 10,
@@ -107,9 +108,8 @@ const products = [
     originalPrice: "$16.48",
     discountedPrice: "$6.48",
     colors: ["bg-blue-500", "bg-red-500", "bg-green-500", "bg-yellow-500"],
-    image: "/images/product-cover-5 (31).png", 
+    image: "/images/product-cover-5 (31).png",
   },
-  
 ];
 
 const Collection = () => {
@@ -120,11 +120,12 @@ const Collection = () => {
           key={product.id}
           className="border rounded-lg p-4 flex flex-col items-center shadow-lg"
         >
-          <img
-          id="#dropShadow"
+          <Image
             src={product.image}
             alt={product.name}
-            className="w-72 h-[450px] object-fit rounded-lg mb-4"
+            width={288} // Example: Adjust as per your requirements
+            height={450} // Example: Adjust as per your requirements
+            className="rounded-lg mb-4 object-cover"
           />
           <h3 className="text-lg font-bold">{product.name}</h3>
           <p className="text-gray-500">{product.department}</p>
@@ -146,45 +147,98 @@ const Collection = () => {
           </div>
         </div>
       ))}
-      
 
-
-<nav aria-label="Page navigation ">
-  <ul className="flex  justify-center items-center mx-auto h-10 text-base">
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-        <span className="sr-only">Previous</span>
-        <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-        </svg>
-      </a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-    </li>
-    <li>
-      <a href="#" aria-current="page" className="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-        <span className="sr-only">Next</span>
-        <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-        </svg>
-      </a>
-    </li>
-  </ul>
-</nav>
-
+      <nav aria-label="Page navigation ">
+        <ul className="flex  justify-center items-center mx-auto h-10 text-base">
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span className="sr-only">Previous</span>
+              <svg
+                className="w-3 h-3 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              1
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              2
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              aria-current="page"
+              className="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+            >
+              3
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              4
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              5
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span className="sr-only">Next</span>
+              <svg
+                className="w-3 h-3 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
